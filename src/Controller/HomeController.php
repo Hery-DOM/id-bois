63fd1f86c934e3b91669d62c4c2a53413ba5c3ab
lapiscine <hery.domenichini@lapiscine.pro>
2019-11-03 11:55:21 +0100

    create gallery all project by category (html + css)

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -7,6 +7,7 @@ namespace App\Controller;
 use App\Repository\ArticleRepository;
 use App\Repository\ProjectCategoryRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 
 class HomeController extends AbstractController
@@ -33,7 +34,8 @@ class HomeController extends AbstractController
      * @Route("/galerie", name="gallery_home")
      * gallery page, to select the category
      */
-    public function galeryHome(ProjectCategoryRepository $projectCategoryRepository, ArticleRepository $articleRepository)
+    public function galleryHome(ProjectCategoryRepository $projectCategoryRepository, ArticleRepository
+    $articleRepository)
     {
         $profile = $articleRepository->findBy(['type'=>7]);
         $categories = $projectCategoryRepository->findAll();
@@ -41,6 +43,26 @@ class HomeController extends AbstractController
         return $this->render('gallery_categories.html.twig',[
             'categories' => $categories,
             'profile' => $profile
+        ]);
+    }
+
+    /**
+     * @Route("/galerie/categorie",name="gallery_category")
+     * gallery page, show products from category selected
+     */
+    public function galleryCategory(ArticleRepository $articleRepository, Request $request, ProjectCategoryRepository $projectCategoryRepository)
+    {
+        $profile = $articleRepository->findBy(['type'=>7]);
+        $categories = $projectCategoryRepository->findAll();
+        //get category's ID
+        $id = $request->query->get('id');
+        //get projects
+        $projects = $articleRepository->findBy(['project_category' => $id]);
+
+        return $this->render('gallery_one_category.html.twig',[
+            'profile' => $profile,
+            'categories' => $categories,
+            'projects' => $projects
         ]);
     }
 
